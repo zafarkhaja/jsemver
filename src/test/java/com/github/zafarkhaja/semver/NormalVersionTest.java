2db8c218525d7906c242e1e4ccadbe3dc707582a
Zafar Khaja <zafarkhaja@gmail.com>
2023-04-11 12:42:53 +0300

    Set locale in NormalVersion.toString() to avoid surprises

--- src/test/java/com/github/zafarkhaja/semver/NormalVersionTest.java
+++ src/test/java/com/github/zafarkhaja/semver/NormalVersionTest.java
@@ -23,6 +23,7 @@
  */
 package com.github.zafarkhaja.semver;
 
+import java.util.Locale;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -196,6 +197,13 @@ class NormalVersionTest {
 
         @Test
         void shouldReturnStringRepresentation() {
+            NormalVersion v = new NormalVersion(1, 2, 3);
+            assertEquals("1.2.3", v.toString());
+        }
+
+        @Test
+        void shouldUseRootLocale() {
+            Locale.setDefault(new Locale("hi", "IN"));
             NormalVersion v = new NormalVersion(1, 2, 3);
             assertEquals("1.2.3", v.toString());
         }
